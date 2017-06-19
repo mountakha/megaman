06f673e7d1556472e76d2aa870ec7dea19172b2d
mountakha <mountakha.djigo@ynov.com>
2017-06-19 06:38:05 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,300 +1,128 @@
+<?php
+
+// Connexion à la base de données
+
+try
+
+{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=test2;charset=utf8', 'root', '');
+
+}
+
+catch(Exception $e)
+
+{
+
+        die('Erreur : '.$e->getMessage());
+
+}
+
+
+// Récupération des 10 derniers messages
+
+$reponse = $bdd->query('SELECT pseudo, message FROM test2 ORDER BY ID DESC LIMIT 0, 10');
+
+
+// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+while ($donnees = $reponse->fetch())
+
+{
+
+    echo '<p>' . '<strong>:' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+
+}
+
+
+$reponse->closeCursor();
+
+
+?>
+
+    </body>
+
+</html>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Megaman</title>
+        <meta name="description" content="Bootstrap contact form with PHP example by BootstrapBay.com.">
+        <meta name="author" content="BootstrapBay.com">
+        <title>Bootstrap Contact Form With PHP Example</title>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="./CSS/business_plan.css" >
+        <link rel="stylesheet" href="./CSS/index.css">
         <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Sirin+Stencil" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     </head>
     <body>
-        <div id="first">
-            <div>
-                <img src="Image/Megaman%20bk-2.png" alt="" class="img-responsive" width="100%">
-            </div>
-            <div >
-                <nav class="navbar navbar-webmaster">
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbar">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="active"><a href="./index.html">INDEX<span class="sr-only">(current)</span></a></li>
-                                <li class="active"><a href="./apropos.html">A PROPOS<span class="sr-only">(current)</span></a></li>
-                                <li class="active"><a href="./business_plan.html">BUSINESS PLAN<span class="sr-only">(current)</span></a></li>
-                                <li class="active"><a href="./contact.php">CONTACT<span class="sr-only">(current)</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div><br>
-        <br>
-        <br>
-        <div class="back">
-            <div class="economie">
-                <p><H1> BUSINESS PLAN</H1><br>
-                    <strong>Objectif: </strong> obtenir le résultat attendu dans le respect du budget et du délai accordée, donc notre première objectif c'est de crée  le jeu avec tous les détails complémentaires sans grande dépense, deuxième objectif c'est la stimulation d'achat.<br>
-                    <br>
-                    <strong>Plan stratégique:</strong>
-                    Premier temps,  investir  le domaine du marketing pour augmenter les telechargements, puis avec le temps quand il aura une bonne base des clients, on va améliorer le jeu: " nouveaux endroits, et ajoutant des vêtements  pour les personage, etc..." <br>
-                    <br>
-                    <strong>Les concurrents:</strong>
-                    Il y a la concurrence  avec les autres retro jeux vidéo. <br>
-
-                    <strong>Au niveau du prix:</strong>
-                    Le prix en Euro pour le téléchargement n’est pas cher mais représente une bonne qualité du jeu.<br>
-
-                    Le prix du jeu est bas car l’objectif c’est de provoquer la lutte avec les concourants.<br>
-                    <br>
-
-                    <strong>LES DETAILS COMPLEMENTAIRES</strong> <br>
-
-                    Les clients en période de jeux peuvent développer le personnage, habiller en équipement, de différentes couleurs etc…<br>
-
-                    Ces grands assortiments en jeu nous permettent de mettre les clients au hasard, pour qu’ils achètent les détails complémentaires pour les personnages. Le prix moyen de modification du jeu coûte 0,50 Euro.<br>
-
-                    Le jeu en ligne nous permet de garder et amener plus de clients.<br>
-                    Ex : pour jouer le jeu, le client invite ses amis qui vont télécharger le jeu.<br>
-
-                    On a deux systèmes de paiement. <br>
-                    La première moitié du jeu est gratuit, mais la deuxième partie sera payante<br>
-                    60% gratuit <br>
-                    40% payant<br>
-                    Les deux premiers mois, le jeu est gratuit, puis il deviendra payant.</p> </div>
-            <br>
-            <br>
-            <div class="table1">
-                <table border="1">
-                    <tr>
-                        <th> </th>
-                        <th>1-3 mois</th>
-                        <th>4 mois</th>
-                        <th>6 mois</th>
-                        <th>12 mois</th>
-                        <th>18 mois</th>
-                        <th>24 mois</th>
-                        <th>36 mois</th>
-                    </tr>
-                    <tr>
-                        <th>Base utilisateur<br>installé</th>
-                        <td>2500</td>
-                        <td>5000</td>
-                        <td>10000</td>
-                        <td>20000</td>
-                        <td>30000</td>
-                        <td>40000</td>
-                        <td>60000</td>
-                    </tr>
-
-                    <tr>
-                        <th>Nombre de <br> téléchargement</th>
-                        <td>    </td>
-                        <td>750</td>
-                        <td>1500</td>
-                        <td>3000</td>
-                        <td>4500</td>
-                        <td>6000</td>
-                        <td>9000</td>
-                    </tr>
-                    <tr>
-                        <th>Taux de <br>
-                            convertion</th>
-                        <td>   </td>
-                        <td>0,15%</td>
-                        <td>0,15%</td>
-                        <td>0,15%</td>
-                        <td>0,15%</td>
-                        <td>0,15%</td>
-                        <td>0,15%</td>
-                    </tr>
-                    <tr>
-                        <th>Payante</th>
-                        <td>     </td>
-                        <td>1,125</td>
-                        <td>2,25</td>
-                        <td>4,5</td>
-                        <td>6,75</td>
-                        <td>9</td>
-                        <td>13,5</td>
-                    </tr>
-                    <tr>
-                        <th>Chiffre d'affaire</th>
-                        <td>   </td>
-                        <td>1500</td>
-                        <td>3000</td>
-                        <td>6000</td>
-                        <td>9000</td>
-                        <td>12000</td>
-                        <td>18000</td>
-                    </tr>
-                    <tr>
-                        <th>Coût variable</th>
-                        <td>   </td>
-                        <td>2500</td>
-                        <td>4800</td>
-                        <td>4200</td>
-                        <td>2700</td>
-                        <td>2800</td>
-                        <td>2200</td>
-                    </tr>
-                    <tr>
-                        <th>Marge variable</th>
-                        <td></td>
-                        <td>-1000</td>
-                        <td>-1800</td>
-                        <td>1800</td>
-                        <td>6300</td>
-                        <td>9200</td>
-                        <td>15800</td>
-                    </tr>
-                </table><br><br>
-            </div>
-            <div class="table2">
-                <table border="1" >
-                    <tr>
-                        <th>     </th>
-                        <th>1 mois</th>
-                        <th>2 mois</th>
-                        <th>3 mois</th>
-                        <th>4 mois</th>
-                        <th>5 mois</th>
-                        <th>6 mois</th>
-                        <th>7 mois</th>
-
-                        <th>9 mois</th>
-                        <th>10 mois</th>
-
-                        <th>12 mois</th>
-                        <th>18 mois</th>
-                        <th>24 mois</th>
-                    </tr><tr>
-                    <th>    Site web (gratuit)      </th>
-                    <td>  </td>
-                    <td>  </td>
-                    <td>  </td>
-                    <td>   </td>
-                    <td>   </td>
-                    <td>   </td>
-                    <td>   </td>
-
-                    <td></td>
-                    <td></td>
-
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    </tr><tr>
-                    <th>Les plateformes</th>
-                    <td></td>
-                    <td>100</td>
-                    <td></td>
-                    <td>1100</td>
-                    <td>1100</td>
-                    <td>1100</td>
-                    <td>1100</td>
-
-                    <td>1100</td>            
-                    <td>1100</td>
-
-                    <td>1100</td>
-                    <td>1100</td>
-                    <td>1100</td>
-                    </tr>
 
 
-                    <tr>
-                        <th>Google</th>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-
-                        <td>100</td>
-                        <td>100</td>
-
-                        <td>100</td>
-                        <td>100</td>
-                        <td>100</td>
-                    </tr>
-                    <tr>
-                        <th>Les jeux <br>partenaires</th>
-                        <td>900</td>
-                        <td>1200</td>
-                        <td>1200</td>
-                        <td>1200</td>
-                        <td>1200</td>
-                        <td>1500</td>
-                        <td>1500</td>
-
-                        <td>1000</td>
-                        <td>1000</td>
-
-                        <td>900</td>
-                        <td>1500</td>
-                        <td>1500</td>
-                    </tr><tr>
-                    <th>Flyers</th>
-                    <td></td>
-                    <td></td>
-                    <td> 100 </td>
-                    <td></td>
-                    <td>  100 </td>
-                    <td></td>
-
-                    <td></td>
-                    <td> 100 </td>
-
-                    <td>  100 </td>
-                    <td>  100 </td>
-                    <td></td>
-                    <td> 200 </td>
-
-                    </tr><tr>
-                    <th>Publicité dans les <br>évènements des jeux</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td> 2000 </td>
-
-                    <td></td>
-                    <td></td>
-
-                    <td> 2000 </td>
-                    <td></td>
-                    <td></td>
-                    </tr>
-                    <tr>
-                        <th>Total C/V </th>
-                        <td> 1000 </td>
-                        <td> 1400 </td>
-                        <td> 1300</td>
-                        <td> 2500 </td>
-                        <td> 2400 </td>
-                        <td> 4800 </td>
-                        <td> 2700 </td>
-                        <td> 2200 </td>
-                        <td> 2300 </td>
-                        <td> 4200 </td>
-                        <td> 2700 </td>
-                        <td> 2800 </td>
-                    </tr>
-                </table>
-                <br> <br></div>
+        <div>
+            <img src="Image/Megaman%20bk-2.png" alt="" class="img-responsive" width="100%">
         </div>
+        <div >
+            <nav class="navbar navbar-webmaster">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="./index.html">INDEX<span class="sr-only">(current)</span></a></li>
+                            <li class="active"><a href="./apropos.html">A PROPOS<span class="sr-only">(current)</span></a></li>
+                            <li class="active"><a href="business_plan.html">BUSINESS PLAN<span class="sr-only">(current)</span></a></li>
+                            <li class="active"><a href="contact.php">CONTACT<span class="sr-only">(current)</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            
+            
+            
+            
+            
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <h1 class="page-header text-center"> Contactez-nous !</h1>
+        <form action="messagerie.php" method="post" style="color:gray ">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <textarea type="text" name="message" id="message" ></textarea><br />
+        <input type="submit" value="Envoyer" />
+         
+        </p>
+
+        </form>
+
+                   
+                   
+            </div>
+            </div>
+        
+
+
+      
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
         <div id="fourth" >
             <div class="container">
                 <div class="col-md-7" >
@@ -312,7 +140,7 @@
                             </div>
                         </form>
                     </div>
-                    <br>
+                    <br><br>
                     <h3> Réseaux Sociaux</h3>
 
                     <nav class="nav icon-set">
@@ -325,22 +153,23 @@
                             </ul>
                         </div>
                     </nav>
-                </div> 
-
+                </div>
             </div>
             <hr>
             <p4 class="lol">Copyright © MegamanRetro 2017.</p4>
-
-        <center>
+            <!-- pagination -->
+            <center>
                 <div class="pagination">
                     <ul id="pagination-clean" position="center">
                         <li class="previous-off">« Précédent</li>
                         <li><a href="./index.html">1</a></li>
                         <li><a href="./apropos.html">2</a></li>
-                        <li><a href="./contact.html">4</a></li>
-                        <li class="next"><a href="./contact.html">Suivant »</a></li>
+                        <li><a href="./business_plan.html">3</a></li>
+                        <li class="next"><a href="./index.html">Suivant »</a></li>
                     </ul></div>
-        </center></div>
+            </center>
+        </div>
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed --><!- Latest compiled and minified JavaScript -->
